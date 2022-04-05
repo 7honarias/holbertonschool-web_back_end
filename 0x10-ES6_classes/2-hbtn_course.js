8d696e30edc088d5a28328fbdcfb84d353072b8c
Jhon Arias <jhon.arias@mercadolibre.com.co>
2022-04-04 22:01:49 -0500

    building

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -38,8 +38,8 @@ class HolbertonCourse {
 
   set students(students) {
     if (
-      Array.isArray(students) &&
-      students.every((s) => typeof s === 'string')
+      Array.isArray(students)
+      && students.every((s) => typeof s === 'string')
     ) {
       this._students = students;
     } else {
